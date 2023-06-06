683762b0e3985215f609bc8dc483e677f7b96e54
Raj Poudel <raj.poudeel@gmail.com>
2023-06-06 14:36:15 +0545

    stating of wrting function for childEL

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,25 @@
 import "./main.css"
+import { useState } from "react";
 function Main(){
+    var inputEl = document.getElementById("input")
+    var btnEl = document.getElementById("btn")
+    var parentEl = document.getElementById("parent")
+    const [todo,settodo] = useState("")
+
+   
+
+
     return(
         <div className="main">
         <h1> Todo app</h1>
         <input type="text" id="input"className="input"placeholder="To-do"/>
+        <button id="btn">Add</button>
+        <div className="parent">
+            <div className="parent--header">
+                <h3>Your to-do list</h3>
+            </div>
+            
+        </div>
         </div>
     );
 }
