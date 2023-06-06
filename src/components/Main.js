ef4adc6b0262fbc2f6a64129889018df7b1ee642
Raj Poudel <raj.poudeel@gmail.com>
2023-06-06 22:11:25 +0545

    setlistdata

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,26 +1,36 @@
 import "./main.css"
 import { useState } from "react";
 function Main(){
-    var inputEl = document.getElementById("input")
-    var btnEl = document.getElementById("btn")
-    var parentEl = document.getElementById("parent")
-    const [todo,settodo] = useState("")
-
-   
-
+    const [listData, setListData] = useState([])
+    const [activity, setActivity] = useState("")
+    function addActivity(){
+        setListData((listData)=>{
+            const newData = [...listData,activity]
+            console.log(newData)
+            setActivity("")
+            return newData;
+        })
+        
+    }
 
     return(
         <div className="main">
         <h1> Todo app</h1>
-        <input type="text" id="input"className="input"placeholder="To-do"/>
-        <button id="btn">Add</button>
-        <div className="parent">
-            <div className="parent--header">
-                <h3>Your to-do list</h3>
-            </div>
-            
-        </div>
+        <input type="text" id="input"className="input"placeholder="To-do" value={activity} onChange={(e)=>setActivity(e.target.value)}/>
+        <button id="btn" onClick={addActivity}>Add</button>
+        <div className="list-heading">Here is your List </div>
+        {listData!=[] && listData.map((data,i)=>{
+            return(
+                <>
+                   <h2 className="addedData" key={i}>
+                     <div className="listData">{data}</div>
+                    </h2> 
+                </>
+            )
+        })
+        }
         </div>
     );
 }
 export default Main;
+
